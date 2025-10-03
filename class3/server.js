9b4f8d39f860612602dcbbcb7c370d03b9ba190a
Rohan yadav <145701101+Rohan4520@users.noreply.github.com>
2025-10-03 16:52:14 +0530

    Add files via upload

---
+++ class3/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const nodemailer = require("nodemailer")
+
+const app =  express()
+
+app.post("/send-mail" , async (req , res )=>{
+    try{
+
+    }
+    catch{
+        
+    }
+});
